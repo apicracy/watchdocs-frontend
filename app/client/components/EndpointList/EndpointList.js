1b410cce9999d4a043368728e2331b1b095834a1
Krzysztof Kałamarski <kkalamarski@gmail.com>
2017-03-08 19:14:43 +0200

    Revert "TUT-26 Modal for new endpoint"

--- app/client/components/EndpointList/EndpointList.js
+++ app/client/components/EndpointList/EndpointList.js
@@ -2,59 +2,35 @@ import React from 'react';
 import styles from './EndpointList.css';
 
 import EndpointListGroup from './EndpointListGroup/EndpointListGroup';
-import Icon from 'components/Icon/Icon';
 
-import AddNewModal from './AddNewModal/AddNewModal';
+const EndpointList = (props) => {
+  const { endpoints, activeGroup, selected } = props;
 
-class EndpointList extends React.Component {
-  static propTypes = {
-    endpoints: React.PropTypes.array,
-    activeGroup: React.PropTypes.string,
-    selected: React.PropTypes.string,
-  };
-
-  componentWillMount() {
-    // cache opened modal
-    this.setState({
-      isOpen: false,
-    });
-  }
-
-  render() {
-    const { endpoints, activeGroup, selected } = this.props;
-
-    return (
-      <div className={styles.root}>
-        <div className={styles.list}>
-          {
-            /* TODO not sure if id will be int or string */
-            endpoints.map(group => (
-              <EndpointListGroup
-                isActive={(`${group.id}` === activeGroup)}
-                isOpen={!!group.isOpen}
-                activeGroup={activeGroup}
-                selected={selected}
-                key={group.id}
-                {...group}
-              />
-            ))
-          }
-        </div>
-        <button
-          onClick={() => { this.setState({ isOpen: true }); }}
-          className={styles.addNewEndpointButton}
-        >
-          Add New <span className={styles.plusButton}><Icon name="plus-circle" /></span>
-        </button>
-
-        <AddNewModal
-          isShow={this.state.isOpen}
-          onSave={() => { this.setState({ isOpen: false }); }}
-          onHide={() => { this.setState({ isOpen: false }); }}
-        />
+  return (
+    <div className={styles.root}>
+      <div className={styles.list}>
+        {
+          /* TODO not sure if id will be int or string */
+          endpoints.map(group => (
+            <EndpointListGroup
+              isActive={(`${group.id}` === activeGroup)}
+              isOpen={!!group.isOpen}
+              activeGroup={activeGroup}
+              selected={selected}
+              key={group.id}
+              {...group}
+            />
+          ))
+        }
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+EndpointList.propTypes = {
+  endpoints: React.PropTypes.array,
+  activeGroup: React.PropTypes.string,
+  selected: React.PropTypes.string,
+};
 
 export default EndpointList;
