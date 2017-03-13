17dcb6ff80cf327c18c61837e9be747e0c27424c
Piotr <piotr.borysowski@gmail.com>
2017-03-13 10:52:02 +0100

    hotfix for production

--- app/client/modals/Modals.js
+++ app/client/modals/Modals.js
@@ -1,19 +1,12 @@
 
 import React from 'react';
-import { connect } from 'react-redux';
 
 import AddNewModal from 'modals/AddNewModal/AddNewModal';
 import EditModal from 'modals/EditModal/EditModal';
 
-@connect(state => state)
-export default class Modals extends React.Component {
-  render() {
-    // Add new modals here:
-    return (
-      <div>
-        <AddNewModal />
-        <EditModal />
-      </div>
-    );
-  }
-}
+export default () => (
+  <div>
+    <AddNewModal />
+    <EditModal />
+  </div>
+);
