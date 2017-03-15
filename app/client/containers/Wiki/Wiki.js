8f1af20405bf691849e3ef0c68b90f45d414073a
Piotr <piotr.borysowski@gmail.com>
2017-03-15 14:59:13 +0100

    DRAFT: JSONSEditor design

--- app/client/containers/Wiki/Wiki.js
+++ app/client/containers/Wiki/Wiki.js
@@ -12,6 +12,9 @@ export default class Wiki extends React.Component {
       base: {},
       baseString: '',
       valid: true,
+      base2: {},
+      baseString2: '',
+      valid2: true,
     });
   }
 
@@ -20,18 +23,39 @@ export default class Wiki extends React.Component {
       base,
       baseString,
       valid,
+      base2,
+      baseString2,
+      valid2,
     } = this.state;
 
     return (
       <div className={styles.container}>
-        <div className={styles.textAreaWrapper}>
-          <textarea className={styles.textArea} value={baseString} onChange={this.onChange} />
-          { !valid && <p>JSON Schema is not valid</p> }
+        <div className={styles.editorWrapper}>
+          <div className={styles.textAreaWrapper}>
+            <h1>Base</h1>
+            <textarea className={styles.textArea} value={baseString} onChange={this.onChange} />
+            { !valid && <p>JSON Schema is not valid</p> }
+          </div>
+          <div className={styles.textAreaWrapper}>
+            <h1>New Draft</h1>
+            <textarea className={styles.textArea} value={baseString2} onChange={this.onChange2} />
+            { !valid2 && <p>JSON Schema is not valid</p> }
+          </div>
         </div>
-        <JSONSEditor
-          className={styles.editor}
-          base={base}
-        />
+        <div className={styles.outputWrapper}>
+          <h1>Output</h1>
+          <div className={styles.outputContainer}>
+            <JSONSEditor
+              base={base}
+              newDraft={base2}
+            />
+          <div className={styles.outputWrapper}>
+              <p className={styles.outputText}>
+                {baseString}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -49,6 +73,22 @@ export default class Wiki extends React.Component {
       });
     } catch (err) {
       this.setState({ valid: false });
+    }
+  }
+
+  onChange2 = (e) => {
+    this.setState({
+      baseString2: e.target.value,
+    });
+
+    try {
+      const obj = JSON.parse(e.target.value);
+      this.setState({
+        base2: obj,
+        valid2: true,
+      });
+    } catch (err) {
+      this.setState({ valid2: false });
     }
   }
 }
