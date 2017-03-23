61b86cb7b3b4bfa864cd4fd4408198b989cea419
Piotr <piotr.borysowski@gmail.com>
2017-03-23 13:25:03 +0100

    fix from PR comments

--- app/client/containers/Wiki/Wiki.js
+++ app/client/containers/Wiki/Wiki.js
@@ -1,43 +1,37 @@
 
 import React from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 
 import styles from './Wiki.css';
 import Button from 'components/Button/Button';
+import DocumentationBlock from 'components/DocumentationBlock/DocumentationBlock';
 
-@connect(state => state)
+// @connect(state => state)
 export default class Wiki extends React.Component {
   render() {
     /* eslint no-undef: 0 */
     // imported in index.html
-    /* eslint react/no-unescaped-entities: 0 */
     tinymce.init({ selector: 'textarea' });
     return (
       <div>
-        <article className={styles.root}>
-          <header className={styles.header}>
-            <div className={styles.title}>Wiki title</div>
-            <div className={styles.description}>This is title of the section we're going
-              to display in documentation and in navigation</div>
-          </header>
-          <main className={styles.blockWrapper}>
-            <div className={styles.gettingStarted}>
-              Getting started
-            </div>
-          </main>
-        </article>
-        <article className={styles.root}>
-          <header className={styles.header}>
-            <div className={styles.title}>Content</div>
-            <div className={styles.description}>This description will
-              appear on your generated public documentation</div>
-          </header>
-          <main className={styles.blockWrapper}>
-            <div className={styles.textarea}>
-              <textArea />
-            </div>
-          </main>
-        </article>
+        <DocumentationBlock
+          title="Wiki title"
+          description="This is title of the section we're going
+            to display in documentation and in navigation."
+        >
+          <div className={styles.gettingStarted}>
+          Getting started
+          </div>
+        </DocumentationBlock>
+
+        <DocumentationBlock
+          title="Content"
+          description="This description will
+            appear on your generated public documentation."
+        >
+          <textArea />
+        </DocumentationBlock>
+
         <div className={styles.buttons}>
           <Button variants={['primary', 'large', 'spaceRight']}>Save</Button>
           <Button variants={['body', 'large']}>Preview</Button>
