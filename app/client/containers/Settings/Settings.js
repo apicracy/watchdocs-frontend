298e786388fbd2e474d5ec7d9050050d443e9be2
Piotr <piotr.borysowski@gmail.com>
2017-03-31 17:35:57 +0200

    TUT-48 Add setting Page

--- app/client/containers/Settings/Settings.js
+++ app/client/containers/Settings/Settings.js
@@ -3,68 +3,65 @@ import styles from './Settings.css';
 
 import Chart from 'components/Chart/Chart';
 import RemoveApp from 'components/RemoveApp/RemoveApp';
-import Button from 'components/Button/Button';
+import TextInput from 'components/Form/TextInput/TextInput';
 
 
 import DocumentationBlock from 'components/DocumentationBlock/DocumentationBlock';
 
 class Settings extends React.Component {
 
-  static defaultProps = {
-    projectName: 'This is a project Name',
-  }
-
   static propTypes = {
-    // params: React.PropTypes.object, // supplied by react-router
-    // dispatch: React.PropTypes.func,
-    projectName: React.PropTypes.string,
+    params: React.PropTypes.object, // supplied by react-router
   }
 
   render() {
     const {
-      projectName,
+      params,
     } = this.props;
 
     return (
       <div className={styles.root}>
-        <DocumentationBlock
-          title="Name"
-          description="This is the name for application that we're going to display."
-        >
-          <div>{projectName}</div>
-        </DocumentationBlock>
-        <div className={styles.row}>
-          <div className={styles.column}>
-            <DocumentationBlock
-              title="APP-Key"
-              description="Lorem ipsum dolor sit amet."
-            >
-              <div>{projectName}</div>
-            </DocumentationBlock>
+        <aside className={styles.aside}>
+          <div className={styles.menu}>
+            <p className={styles.active}>Application setup</p>
+            <p>User access</p>
+            <p>Security schemas</p>
           </div>
-          <div className={styles.column}>
-            <DocumentationBlock
-              title="APP-ID"
-              description="Lorem ipsum dolor sit amet."
-            >
-              <div>{projectName}</div>
-            </DocumentationBlock>
+        </aside>
+        <div className={styles.container}>
+          <DocumentationBlock
+            title="Name"
+            description="This is the name for application that we're going to display."
+          >
+            <TextInput value={params.project_name} />
+          </DocumentationBlock>
+          <div className={styles.row}>
+            <div className={styles.column}>
+              <DocumentationBlock
+                title="APP-Key"
+                description="Lorem ipsum dolor sit amet."
+              >
+                <TextInput value="7f7sdfshjf43743fbhj" />
+              </DocumentationBlock>
+            </div>
+            <div className={styles.column}>
+              <DocumentationBlock
+                title="APP-ID"
+                description="Lorem ipsum dolor sit amet."
+              >
+                <TextInput value="shadow-of-forest-5534" />
+              </DocumentationBlock>
+            </div>
           </div>
+          <DocumentationBlock
+            title="Collected data"
+            description="We're currently tracking request from your application."
+          >
+            <Chart />
+          </DocumentationBlock>
+
+          <RemoveApp onClick={() => {}} />
         </div>
-        <DocumentationBlock
-          title="Collected data"
-          description="We're currently tracking request from your application."
-        >
-          <Chart />
-        </DocumentationBlock>
-
-        <RemoveApp onClick={() => {}} />
-
-        <div className={styles.buttons}>
-          <Button variants={['primary', 'large', 'spaceRight']}>Save</Button>
-          <Button variants={['body', 'large']}>Preview</Button>
-        </div>
-
       </div>
     );
   }
