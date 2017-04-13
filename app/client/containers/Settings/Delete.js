2843cf6f2a8fffa1cec281180647751e1b50f822
Piotr <piotr.borysowski@gmail.com>
2017-04-13 11:19:13 +0200

    TUT-141 [FEATURE] Add delete page

---
+++ app/client/containers/Settings/Delete.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import styles from './Settings.css';
+
+import DocumentationBlock from 'components/DocumentationBlock/DocumentationBlock';
+
+@connect(store => ({
+  projectName: store.projects.activeProject.name,
+}))
+class Delete extends React.Component {
+
+  static propTypes = {
+    projectName: React.PropTypes.string, // supplied by react-router
+  }
+
+  render() {
+    const {
+      projectName,
+    } = this.props;
+
+    return (
+      <div className={styles.container}>
+        <DocumentationBlock
+          title={`Delete "${projectName}" project`}
+        >
+          <p className={styles.text}><b>Warning</b> Deleting this project will cause all of
+            its associated data to be deleted immediately.</p>
+          <p className={styles.text}><b>This action can not be undone.</b></p>
+          <button className={styles.deleteButton}>
+            I understand -- Delete this project ({projectName}) and its data</button>
+        </DocumentationBlock>
+      </div>
+    );
+  }
+}
+
+export default Delete;
