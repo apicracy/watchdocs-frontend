6321b2f0f699a5598b5767532096bfdfcb678347
Krzysztof <k.szromek@exlabs.pl>
2017-06-02 11:29:58 +0200

    Refactor routes

--- app/client/routes.js
+++ app/client/routes.js
@@ -6,7 +6,7 @@ import About from 'containers/About';
 import NoMatch from 'containers/NoMatch';
 
 /* import pages */
-import Loading from 'containers/Loading/Loading';
+import LoadingIndicator from 'components/LoadingIndicator/LoadingIndicator';
 import DocumentationViewer from 'containers/DocumentationViewer/DocumentationViewer';
 import Projects from 'containers/Projects/Projects';
 import Editor from 'containers/Editor/Editor';
@@ -18,6 +18,7 @@ import ResponseEditor from 'containers/ResponseEditor/ResponseEditor';
 import Settings from 'containers/Settings/Settings';
 import Login from 'containers/Login/Login';
 import Signup from 'containers/Signup/Signup';
+import NewProjectWizard from 'containers/Projects/NewProjectWizard';
 
 const requireAuth = (nextState, replace) => {
   const { pathname } = nextState.location;
@@ -37,41 +38,47 @@ const requireAuth = (nextState, replace) => {
   }
 };
 
+const requireProjects = (nextState, replace) => {
+  debugger;
+}
+
 const Routes = (
   <Route>
-    <Route exact path="/" component={AppLayout} onEnter={requireAuth}>
-      <IndexRoute component={Loading} />
-    </Route>
-
-    <Route path="/project-manager" component={Projects} onEnter={requireAuth} />
     <Route path="/login" component={Login} />
     <Route path="/signup" component={Signup} />
 
-    <Route path=":project_name" component={AppLayout} onEnter={requireAuth}>
-      <IndexRedirect to="editor" />
-
-      <Route path="project-not-found" component={NoMatch} />
-
-      <Route path="editor">
-        <IndexRoute component={Editor} />
-        <Route component={Editor}>
-          <Route path="document/:document_id" component={DocumentEditor} />
-          <Route path=":group_id" component={GroupEditor} />
-          <Route path=":group_id/endpoint/:endpoint_id" component={EndpointEditor} />
-          <Route path=":group_id/endpoint/:endpoint_id/request" component={RequestEditor} />
-          <Route path=":group_id/endpoint/:endpoint_id/response/:response_id" component={ResponseEditor} />
-        </Route>
+    <Route onEnter={requireAuth}>
+      <Route exact path="/" component={AppLayout}>
+        <IndexRoute component={LoadingIndicator} />
       </Route>
 
-      <Route path="documentation" component={DocumentationViewer} />
+      <Route path="new_project" component={NewProjectWizard} />
 
-      <Route path="about" component={About} />
+      <Route onEnter={requireProjects}>
+        <Route path=":project_name" component={AppLayout}>
+          <IndexRedirect to="editor" />
 
-      <Route path="settings" component={Settings} />
-    </Route>
+          <Route path="editor">
+            <IndexRoute component={Editor} />
+            <Route component={Editor}>
+              <Route path="document/:document_id" component={DocumentEditor} />
+              <Route path=":group_id" component={GroupEditor} />
+              <Route path=":group_id/endpoint/:endpoint_id" component={EndpointEditor} />
+              <Route path=":group_id/endpoint/:endpoint_id/request" component={RequestEditor} />
+              <Route path=":group_id/endpoint/:endpoint_id/response/:response_id" component={ResponseEditor} />
+            </Route>
+          </Route>
 
-    <Route path="*" component={AppLayout} onEnter={requireAuth}>
-      <IndexRoute component={NoMatch} />
+          <Route path="documentation" component={DocumentationViewer} />
+          <Route path="about" component={About} />
+          <Route path="settings" component={Settings} />
+        </Route>
+        <Route path="/project-manager" component={Projects} />
+      </Route>
+
+      <Route path="*" component={AppLayout}>
+        <IndexRoute component={NoMatch} />
+      </Route>
     </Route>
   </Route>
 );
